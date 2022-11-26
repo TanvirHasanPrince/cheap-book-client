e08193495cd00453b9997f793323ad6e43e36398
Tanvir Hasan Prince <t.hasan.prince@gmail.com>
2022-11-26 12:54:04 +0600

    navbar

---
+++ src/Pages/Books/Books/BooksPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BooksPage = () => {
+ return (
+  <div>
+   <h3>I am the boooks page</h3>
+  </div>
+ );
+};
+
+export default BooksPage;
---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Blog from "../../Pages/Blog/Blog";
+import BooksPage from "../../Pages/Books/Books/BooksPage";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+       path: '/login', 
+       element: <Login></Login>
+      },
+      {
+       path: '/blog', 
+       element: <Blog></Blog>
+      },
+      {
+       path: '/books', 
+       element: <BooksPage></BooksPage>
+      }
+
+    ],
+  },
+]);
